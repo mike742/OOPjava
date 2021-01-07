4ab8a8a7cd3b8d37bb272bda5e3dda61151c5e0b
MikeZorin <mykhailo.zorin@gmail.com>
2021-01-07 09:58:49 -0600

    added classes

---
+++ src/DriverLamp.java
@@ -0,0 +1,13 @@
+
+public class DriverLamp {
+
+	public static void main(String[] args) {
+		
+		Lamp l1 = new Lamp();
+		
+		l1.turnOn();
+		l1.turnOff();
+		
+	}
+
+}
--- src/OOP.java
+++ src/OOP.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 public class OOP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		/*
 		double d1 = 63;
 		double d2 = 65;
 		
@@ -13,7 +15,7 @@ public class OOP {
 		double res = 10 * Math.log10(p1 + p2);
 		
 		System.out.println("res = " + res);
-		
+		*/
 		/*
 		
 		Enter a variable name (q to quit): street address2
@@ -57,12 +59,80 @@ public class OOP {
 		// phone("1-816-7412000");
 		// phone("86-131-12345678");
 		
-		String pn = "1-816-7412000";
-		String[] parts = pn.split("-");
+		//String pn = "1-816-7412000";
+		//String[] parts = pn.split("-");
 		
-		System.out.println(parts[0]);
-		System.out.println(parts[1]);
-		System.out.println(parts[2]);
+		//System.out.println(parts[0]);
+		//System.out.println(parts[1]);
+		//System.out.println(parts[2]);
+		/*
+		Scanner sn = new Scanner(System.in);
+		String pn; // = "";
+		
+		
+		while(true) {
+			System.out.println("Enter var name (q): ");
+			pn = sn.nextLine();
+			
+			if(pn.compareTo("q") == 0) {
+				break;
+			}
+			
+			int res2 = varNameChecker(pn); // 3, 2, 1
+			
+			switch(res2) {
+			case 1: 
+				System.out.println("Illegal");
+				break;
+			case 2: 
+				System.out.println("poor styled");// 
+				break;
+			case 3: 
+				System.out.println("Good");
+				break;
+			}
+			
+			
+			String[] parts = pn.split("-");
+			
+			System.out.println(parts[0]);
+			System.out.println(parts[1]);
+			System.out.println(parts[2]);
+			
+		}
+		*/
+		Scanner sn = new Scanner(System.in);
+		String vn ="";
+
+		while(true) {
+			
+			System.out.println ("Enter var name (q): ");
+			vn = sn.nextLine();
+			
+			System.out.println("With space: " + vn);
+			
+			
+			if (vn.compareTo("q") == 0) {
+				break;
+			}
+
+			int res2= varNameChecker(vn); //3,2,1
+
+			switch (res2) {
+				case 1:
+					System.out.println( "Illegal") ;
+					break;
+				case 2:
+					System.out.println( "Legal, but uses poor style") ;
+					break;
+				case 3:
+					System.out.println( "Good") ;
+					break;
+			}
+
+		}
+		sn.close();
+		System.out.println("Have a nice day!");
 	}
 	
 	
@@ -94,28 +164,30 @@ public class OOP {
 		System.out.println("cc: " + cc);
 		System.out.println("ac: " + ac);
 		System.out.println("pn: " + pn);
+		
+		
 	}
 	
 	// public static void 
 	public static int varNameChecker(String input) {
 
 		int res = 3;
-		
+
 		for(int i = 0; i < input.length() && res != 1; ++i) {
-			
-			char c = input.charAt(i);
-			
-			if( (Character.isDigit(c) && i == 0) ||
-				!( Character.isLetterOrDigit(c) || c == '$' )) {
-				res = 1;
-			}
-			
-			if( (Character.isLetter(c) && Character.isUpperCase(c) && i == 0) ||
-				c == '$') {
-				res = 2;
-			}
+
+		char c = input.charAt(i);
+
+		if( (Character.isDigit(c) && i == 0) ||
+		!( Character.isLetterOrDigit(c) || c == '$' )) {
+		res = 1;
 		}
-		
+
+		if( (Character.isLetter(c) && Character.isUpperCase(c) && i == 0) ||
+		c == '$') {
+		res = 2;
+		}
+		}
+
 		return res;
 	}
 
