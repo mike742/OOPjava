099633537c1986b1caa133eda3a36d08acf66bf2
MikeZorin <mykhailo.zorin@gmail.com>
2021-01-11 09:54:31 -0600

    added reference and value types

--- src/CarDriver.java
+++ src/CarDriver.java
@@ -4,12 +4,41 @@ public class CarDriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int a = 10;
+		int b = 10;
+
+		if(a == b) {
+			System.out.println("a and b are the same");
+		}
+		else {
+			System.out.println("a and b are NOT the same");
+
+		}		
+		
+		String str1 = new String("string number 1");
+		String str2 = new String("string number 1");
+		
+		if( str1.equals(str2) ) {
+			System.out.println("str1 and str2 are the same");
+		}
+		else {
+			System.out.println("str1 and str2 are NOT the same");
+
+		}
+		
+		
+		
+		
+		
+		
 		Car car1 = new Car(8000.33f, "silver");
-
+		Car car2 = new Car(777, "red");
+		
+		car2 = car1;
+		
+		car2.setColour("white");
+		
 		car1.display();
-
-		Car car2 = new Car(2500.0f, "white");
-
 		car2.display();
 
 	}
