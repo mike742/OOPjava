8e9ebb70bba611651fa6764e08d588a3ad523f66
MikeZorin <mykhailo.zorin@gmail.com>
2021-01-15 11:31:18 -0600

    added isValid static method

--- src/IpAddress.java
+++ src/IpAddress.java
@@ -8,11 +8,38 @@ public class IpAddress {
 		// 216.27.6.136 => 3'.' (0 - 255)
 		String[] res = dottedDecimal.split("[.]");
 		
-		firstOctet = Integer.parseInt(res[0]); 
+		firstOctet = Integer.parseInt(res[0]); // (0 - 255)
 		secondOctet = Integer.parseInt(res[1]);
 		thirdOctet = Integer.parseInt(res[2]);
 		fourthOctet = Integer.parseInt(res[3]);
 		
 		System.out.println(firstOctet + " " + secondOctet + " " + thirdOctet + " " + fourthOctet);
+	}
+	
+	public static boolean isValid(String value) {
+		boolean res = true;
+		int numOfDots = 0;
+		
+		// 1
+		for(int i = 0; i < value.length(); ++i) {
+			if(value.charAt(i) == '.') {
+				numOfDots++;
+			}
+		}
+		System.out.println("numOfDots = " + numOfDots);
+		if(numOfDots != 3) {
+			return false;
+		}
+		
+		// 2 
+		for(int i = 0; i < value.length(); ++i) {
+			char c = value.charAt(i);
+			
+			if(!(c == '.' || Character.isDigit(c))) {
+				return false;
+			}
+		}	
+		
+		return res;
 	}
 }
--- src/IpAddressDriver.java
+++ src/IpAddressDriver.java
@@ -1,10 +1,45 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class IpAddressDriver {
 
 	public static void main(String[] args) {
 
-		IpAddress ip = new IpAddress("216.22.22.22");
+		Scanner sn = new Scanner(System.in);
+		String ip = " ";
+		
+		System.out.println("Enter your IP address: ");
+		ip = sn.next();
+		
+		if(IpAddress.isValid(ip)) {
+			IpAddress address = new IpAddress(ip);
+			
+		}
+		else {
+			System.out.println(" Invalid input!!!!!");
+		}
+		
+		/*
+		boolean isValid;
+
+		do {
+			System.out.println("Enter your IP address: ");
+			num = sn.next();
+			if (num.contains(".")) {
+
+				IpAddress ip = new IpAddress(num);
+				isValid = true;
+			} else {
+				System.out.println("Please enter validnumber");
+				isValid = false;
+				sn.next();
+			}
+
+		} while (!(isValid));
+		 */
+		
+		System.out.println("Have a nice day!");
+		sn.close();
 
 	}
 
